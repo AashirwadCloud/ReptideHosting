6dbaeab8a787296a9a2eda248b6d219b8c275c0b
AashirwadCloud <159162740+AashirwadCloud@users.noreply.github.com>
2025-06-26 21:09:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
@@ -8,11 +8,11 @@ const firebaseConfig = {
   projectId: "data-5f291",
   storageBucket: "data-5f291.appspot.com",
   messagingSenderId: "397689315534",
-  appId: "1:397689315534:web:88980e22fd9b4c8456f09f",
-  measurementId: "G-2X20MH4M4B"
+  appId: "1:397689315534:web:88980e22fd9b4c8456f09f"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { auth };
+
