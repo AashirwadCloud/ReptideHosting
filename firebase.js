deb07c6bae04b24744abf5e4a244b3776d43100c
AashirwadCloud <159162740+AashirwadCloud@users.noreply.github.com>
2025-06-28 22:26:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { db } from './firebase.js';
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
